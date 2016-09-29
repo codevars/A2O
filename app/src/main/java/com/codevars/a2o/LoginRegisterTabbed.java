5c11f8bf33c3251f78817cb1b99fc8969735002f
Rishabh <pingrishabh@gmail.com>
2016-09-30 02:15:14 +0530

    Initial commit

--- app/src/main/java/com/codevars/a2o/LoginRegisterTabbed.java
+++ app/src/main/java/com/codevars/a2o/LoginRegisterTabbed.java
@@ -1,14 +1,19 @@
 package com.codevars.a2o;
 
+import android.graphics.Color;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.codevars.a2o.Adapters.ViewPagerAdapter;
 
 
 public class LoginRegisterTabbed extends AppCompatActivity {
+
 
     private Toolbar toolbar;
 
@@ -16,10 +21,12 @@ public class LoginRegisterTabbed extends AppCompatActivity {
 
     private ViewPager viewpager;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_register_tabbed);
+
 
         viewpager = (ViewPager) findViewById(R.id.viewpager);
 
@@ -29,6 +36,20 @@ public class LoginRegisterTabbed extends AppCompatActivity {
 
         tablayout.setupWithViewPager(viewpager);
 
+        hideStatusBar();
+
+
+    }
+
+
+
+    private void hideStatusBar() {
+
+        Window window = getWindow();
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        window.setStatusBarColor(Color.BLACK);
 
     }
 
